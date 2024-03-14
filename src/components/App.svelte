0942f19ed3268a021af0fb83cb62526e67cdaeff
DonaldTaggart <dtaggart@ucsd.edu>
2024-03-14 16:48:50 -0700

    Updating App.Svelte to tell a story and adding geo.json

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -11,6 +11,10 @@
     <body style="margin:100px">
       <h1>Two Methods for Measuring Economic Health</h1>
       <h2>Gross Domestic Product per Capita and the Gini Coefficient</h2>
+
+      <div>
+        <p>As data scientists, it is our responsibility to explore whatever data may apply towards the concept we look to better understand. This means putting our domain knowledge to good use by determining which metrics are sufficient for measuring a particular trait and which can be helpful but only tell a limited part of the story in which we aim to discover. Let's see how this idea applies in the case of the strength of each country's economy!</p>
+      </div>
   
       <div>
         <h3>Gross Domestic Product (GDP)</h3>
@@ -57,17 +61,6 @@
         <script src="https://d3js.org/d3-scale-chromatic.v1.min.js"></script>
         <script src="https://d3js.org/d3-geo-projection.v2.min.js"></script>
       </div>
-  
-      <div>
-        <p>Comparing the Two Measurements:</p>
-        <ul>
-          <li>There are pros and cons to using each of the measurements.</li>
-          <li>GDP measures economic activity well, but fails to provide information on what proportion of the population is involved in said activity.</li>
-          <li>The Gini index descrives wealth distribution well, but does not indicate the level of either cumulative or average wealth.</li>
-          <li>A more accurate picture can be acquired through looking at both measures.</li>
-        </ul>  
-      </div>
-
 
       <br>
       <br>
@@ -84,6 +77,7 @@
 
 
       <div>
+        <h2>Do these two metrics always agree for each country?</h2>
         <label for="yearSelect">Select Year to Compare:</label>
         <select id="yearSelect">   
           <option value="1985" selected>1985</option>
@@ -134,6 +128,22 @@
       <br>
       <br>
       <br>
+
+      <div>
+        <p>Notice how weak the correlation between the two metrics is throughout the years?</p>
+        <p>If only one metric was satisfactory for measuring an economy's health, the correlation between the two would be nearly perfect given they would always be "agreeing" on the relative health of each economy when compared to that of another. However, it can be seen that this is not the case given it is fairly common for a country to have a more impressive GDP per capita than another (a greater value along the y-axis) but have a less impressive gini index (a greater value along the x-axis).</p> 
+      </div>
+
+      <div>
+        <p>So why consider both metrics?</p>
+        <ul>
+          <li>GDP measures economic activity well, but fails to provide information on what proportion of the population is involved in said activity.</li>
+          <li>The Gini index descrives wealth distribution well, but does not indicate the level of either cumulative or average wealth.</li>
+          <li>A more accurate picture can be depicted by looking at both measures because each of their pros can be extracted and cons negated.</li>
+        </ul>
+        <p>This economics example is a reminder of why feature selection through the use of EDA is such as important first step of the data science process.</p>
+      </div>
+
       <br>
       <br>
       <br>
@@ -142,14 +152,14 @@
       <br>
   
       <div>
-        <h2>Demo Writeup</h2>
+        <h2>Demo Writeup from 3/1/2024</h2>
         <h3>What have you done so far?</h3>
         <p>This demo contains two of our planned visualizations and pulls from two datasets we cleaned. One is a scatterplot that displays the gini index and GDP per capita data for each country for any year chosen by the user. The other is the Lorenz curve which is shaped by user input of the gini index. This demo also contains much of the background information and analysis that will make up the final product's text.</p>
         <h3>What will be the most challenging of your project to design and why?</h3>
         <p>The most challenging aspects of finishing the project will be adding more interactive elements to our visualizations without them interfering with how said visualizations are initially displayed. Making these visualizations (and therefore interactive components) aesthetically pleasing without impeding on how effectively they communicate our message will also be challenging due in part to the subjective nature of many of the design decisions that will need to be made. Significant time may also need to be invested into forming more of a storyline that helps viewers explore our topics and better understand the economics-related message we our aiming to convey. Continuing to become familiar with d3.js syntax as well as other technical skills (such as working with GeoJSON data) will also take some time in order to accomplish the previously-mentioned tasks.</p>
-      </div>    
-
-
+        <h2>Introduction Video from 3/8/2024</h2>
+        <iframe width="800" height="600" src="https://www.youtube.com/embed/Fv5OA3sOYcE"></iframe>
+      </div>
     </body>
     </html>
 </main>
@@ -461,11 +471,12 @@ function drawScatter(filteredData) {
     }
     h2 {
         font-size: 12pt;
-        padding:20px
+        padding:20px;
+        text-align:center;
     }
     h3 {
         font-size: 12pt;
-        padding:20px
+        padding:10px;
     }
     p {
         font-size: 10pt;
